f4e6cba9cf2b19f8c2040c615e87d4652d8a1773
Csoyee <ithdli@naver.com>
2018-03-24 17:34:22 +0900

    add Overlay Marker

--- app/src/main/java/com/example/ddubeok/MainActivity.java
+++ app/src/main/java/com/example/ddubeok/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ddubeok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 import com.nhn.android.maps.NMapActivity;
@@ -10,6 +11,10 @@ import com.nhn.android.maps.NMapController;
 import com.nhn.android.maps.NMapView;
 import com.nhn.android.maps.maplib.NGeoPoint;
 import com.nhn.android.maps.nmapmodel.NMapError;
+import com.nhn.android.maps.overlay.NMapPOIdata;
+import com.nhn.android.mapviewer.overlay.NMapOverlayManager;
+import com.nhn.android.mapviewer.overlay.NMapPOIdataOverlay;
+import com.nhn.android.mapviewer.overlay.NMapResourceProvider;
 
 
 public class MainActivity extends NMapActivity {
@@ -18,6 +23,13 @@ public class MainActivity extends NMapActivity {
 
     NMapView mMapView;
     NMapController mMapController;
+
+    // 지도 위 오버레이 객체 드로잉에 필요한 리소스 데이터 제공 클래스
+    NMapResourceProvider mMapViewerResourceProvider;
+    // 오버레이 객체 관리 클래스
+    NMapOverlayManager mOverlayManager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +46,27 @@ public class MainActivity extends NMapActivity {
 
         // initialize map view
         mMapView.setClickable(true);
-
-
         mMapController = mMapView.getMapController();
 
+        mMapViewerResourceProvider = new NMapViewerResourceProvider(this);
+        mOverlayManager = new NMapOverlayManager(this, mMapView, mMapViewerResourceProvider);
+
+        testOverlayMarker();
     }
+
+
+    private void testOverlayMarker() {
+        int markerID = NMapPOIflagType.PIN;
+        NMapPOIdata poIData = new NMapPOIdata(2, mMapViewerResourceProvider);
+        poIData.beginPOIdata(2);
+        poIData.addPOIitem(128.3925046, 36.1454420, "marker1", markerID, 0);
+        poIData.addPOIitem(128.3915046, 36.1354420, "marker2", markerID, 0);
+        poIData.endPOIdata();
+
+        NMapPOIdataOverlay poIdataOverlay = mOverlayManager.createPOIdataOverlay(poIData, null);
+        poIdataOverlay.showAllPOIdata(0);
+    }
+
 
     public void onMapInitHandler(NMapView mapView, NMapError errorInfo) {
         if (errorInfo == null) { // success
